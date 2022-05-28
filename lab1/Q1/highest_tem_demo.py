6b9037ea650c1bfd66eb6eb6e9f0f17480be5137
rui0027 <90400187+rui0027@users.noreply.github.com>
2022-05-29 01:04:51 +0200

    Update highest_tem_demo.py

--- lab1/Q1/highest_tem_demo.py
+++ lab1/Q1/highest_tem_demo.py
@@ -1,4 +1,4 @@
-
+### 1)
 
 from pyspark import SparkContext
 
@@ -8,19 +8,77 @@ temperature_file = sc.textFile("BDA/input/temperature-readings.csv")
 lines = temperature_file.map(lambda line: line.split(";"))
 
 # (key, value) = (year,temperature)
-year_temperature = lines.map(lambda x: (x[1][0:4], (x[0],float(x[3]))))
+year_temperature = lines.map(lambda x: (x[1][0:4],float(x[3])))
 
 #filter
 year_temperature = year_temperature.filter(lambda x: int(x[0])>=1950 and int(x[0])<=2014)
 
 #Get max
-max_temperatures = year_temperature.reduceByKey(lambda a,b: (a[0], max(a[1], b[1])))
+max_temperatures = year_temperature.reduceByKey(lambda a,b: a if a>=b else b)
+max_temperatures = max_temperatures.sortBy(ascending = False, keyfunc=lambda k: k[1])
 
 #print(max_temperatures.collect())
 
-# merge the output files into one file and order the data by the max temperatures
-max_t = max_temperatures.coalesce(1).sortBy(ascending = False, keyfunc=lambda k: k[1][1])
 # Following code will save the result into /user/ACCOUNT_NAME/BDA/output folder
-max_t.saveAsTextFile("BDA/output")
+max_temperatures.saveAsTextFile("BDA/output")
 
+
+
+
+### 2)
+
+from pyspark import SparkContext
+
+sc = SparkContext(appName = "exercise 1")
+# This path is to the file on hdfs
+temperature_file = sc.textFile("BDA/input/temperature-readings.csv")
+lines = temperature_file.map(lambda line: line.split(";"))
+
+# (key, value) = (year-month,temperature)
+year_temperature = lines.map(lambda x: (x[1][0:7],float(x[3])))
+
+#filter
+year_temperature = year_temperature.filter(lambda x: int(x[0][0:4])>=1950 and int(x[0][0:4])<=2014 and x[1]>10)
+
+#map
+year_temperature = year_temperature.map(lambda x: (x[0],1))
+
+#count
+count_temperatures=year_temperature.reduceByKey(lambda a,b: a+b)
+
+#print(max_temperatures.collect())
+
+# Following code will save the result into /user/ACCOUNT_NAME/BDA/output folder
+count_temperatures.saveAsTextFile("BDA/output")
+
+
+
+
+### Repeat the exercise,this time taking only distinct readings from each station.
+from pyspark import SparkContext
+
+sc = SparkContext(appName = "exercise 1")
+# This path is to the file on hdfs
+temperature_file = sc.textFile("BDA/input/temperature-readings.csv")
+lines = temperature_file.map(lambda line: line.split(";"))
+
+# (key, value) = ((year-month,station),temperature)
+year_temperature = lines.map(lambda x: ((x[1][0:7],x[0]),float(x[3])))
+
+#filter
+year_temperature = year_temperature.filter(lambda x: int(x[0][0][0:4])>=1950 and int(x[0][0][0:4])<=2014 and x[1]>10)
+
+#count
+year_temperature = year_temperature.reduceByKey(lambda a,b: 1)
+count_temperatures=year_temperature.reduceByKey(lambda a,b: a+b)
+
+#print(max_temperatures.collect())
+
+# Following code will save the result into /user/ACCOUNT_NAME/BDA/output folder
+count_temperatures.saveAsTextFile("BDA/output")
+
+
+
+
+### 3)
 
