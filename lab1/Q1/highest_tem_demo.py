1d10cdbc1c8f04480fed3138d13a3b96320384c7
rui0027 <90400187+rui0027@users.noreply.github.com>
2022-05-31 14:56:15 +0200

    Update highest_tem_demo.py

--- lab1/Q1/highest_tem_demo.py
+++ lab1/Q1/highest_tem_demo.py
@@ -25,6 +25,30 @@ max_temperatures.saveAsTextFile("BDA/output")
 
 
 
+from pyspark import SparkContext
+
+sc = SparkContext(appName = "exercise 1")
+# This path is to the file on hdfs
+temperature_file = sc.textFile("BDA/input/temperature-readings.csv")
+lines = temperature_file.map(lambda line: line.split(";"))
+
+# (key, value) = (year,temperature)
+year_temperature = lines.map(lambda x: (x[1][0:4],float(x[3])))
+
+#filter
+year_temperature = year_temperature.filter(lambda x: int(x[0])>=1950 and int(x[0])<=2014)
+
+#Get max
+min_temperatures = year_temperature.reduceByKey(lambda a,b: a if a<=b else b)
+min_temperatures = min_temperatures.sortBy(ascending = False, keyfunc=lambda k: k[1])
+
+#print(min_temperatures.collect())
+
+# Following code will save the result into /user/ACCOUNT_NAME/BDA/output folder
+min_temperatures.saveAsTextFile("BDA/output")
+
+
+
 ### 2)
 
 from pyspark import SparkContext
