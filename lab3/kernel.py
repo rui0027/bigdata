69ee9432f6d52d7c7dba1f58d9d70018d94c6eb4
rui0027 <90400187+rui0027@users.noreply.github.com>
2022-06-03 16:40:26 +0200

    Update kernel.py

--- lab3/kernel.py
+++ lab3/kernel.py
@@ -18,15 +18,15 @@ def haversine(lon1, lat1, lon2, lat2):
   km = 6367 * c
   return km
 
-h_distance = 1# Up to you
-h_date = 7# Up to you
-h_time = 0.5# Up to you
+h_distance = 5# Up to you
+h_date = 3# Up to you
+h_time = 1# Up to you
 a = 58.4274 # Up to you
 b = 14.826 # Up to you
 date = "2014-07-04" # Up to you
 
 stations = sc.textFile("BDA/input/stations.csv")
-temps = sc.textFile("BDA/input/temperature-readings-small.csv")
+temps = sc.textFile("BDA/input/temperature-readings.csv")
 
 # Your code here
 stations_lines = stations.map(lambda line: line.split(";"))
@@ -38,23 +38,25 @@ stations_data = stations_lines.map(lambda x: (x[0],(float(x[3]),float(x[4]))))
 #(station_num,distance)
 distance_stations = stations_data.map(lambda x: (x[0],haversine(b,a,x[1][1],x[1][0])))
 #broadcast distance data
-broadcast_distance = sc.broadcast(distance_stations.collect())
+broadcast_distance = sc.broadcast(dict(distance_stations.collect()))
 
 # gaussian kernel
 def gk(x,h):
-  return exp(-x**2/(2*h**2))
-
-#map the temp data
-#(station_num,date,time,temperature)
-temp_data = temp_lines.map(lambda x: (x[0],datetime(int(x[1][0:4]),int(x[1][5:7]),int(x[1][8:10])),x[2],float(x[3])))
-
-#filter the dates which are no later than the date of interest(including the day)
-datetime_pre = datetime(int(date[0:4]),int(date[5:7]),int(date[8:10]))
-temp_data_pre = temp_data.filter(lambda x: x[1]<=datetime_pre)
-
-#the hour doesn't influence the kernel function of distance and day, so we can preprocess these 2 kernel functions
-#(date,time,distance_kernel,day_kernel,temperature)
-kernel_fun_pre = temp_data_pre.map(lambda x: (x[1],x[2],gk(dict(broadcast_distance.value)[x[0]],h_distance),gk((datatime_pre-x[1]).days,h_date),x[3])).cache()
+	return exp(-x**2/(2*h**2))
+# distance kernel
+def kernel_distance(station_num):
+  return gk(broadcast_distance.value[station_num],h_distance)
+  
+def kernel_day(day_1,day_2):
+  return gk(abs((day_1-day_2).days)%365,h_date)
+  
+def kernel_hour(time_1,time_2):
+	return gk(abs((time_1-time_2).total_seconds())/3600,h_time)
+	
+#preprocess
+#(station_num,datetime,temperature)
+temp_data = temp_lines.map(lambda x: (x[0],datetime(int(x[1][0:4]),int(x[1][5:7]),int(x[1][8:10]),int(x[2][0:2]),int(x[2][3:5]),int(x[2][6:8])),float(x[3])))
+temp_data = temp_data.filter(lambda x: x[1]<=(datetime(int(date[0:4]),int(date[5:7]),int(date[8:10]),0,0,0)+timedelta(days=1))).cache()
 
 # prediction
 prediction_temp={}
@@ -65,16 +67,16 @@ for time in ["24:00:00", "22:00:00", "20:00:00", "18:00:00", "16:00:00", "14:00:
   	datetime_interest = datetime(int(date[0:4]),int(date[5:7]),int(date[8:10]),0,0,0)+timedelta(days=1)
   else:
   	datetime_interest = datetime(int(date[0:4]),int(date[5:7]),int(date[8:10]),int(time[0:2]),int(time[3:5]),int(time[6:8]))
-  kernel_fun = kernel_fun_pre.filter(lambda x: x[0].replace(hour=x[1][0:2],minute=x[1][3:5],second=x[1][6:8])<datetime_interest)
-  #(distance_kernel+day_kernel+hour_kernel,temperature)
-  kernel_all = kernel_fun.map(lambda x: (x[2]+x[3]+gk(abs((datetime(0,0,0,time[0:2],time[3:5],time[6:8])-datetime(0,0,0,x[1][0:2],x[1][3:5],x[1][6:8])).total_seconds())/3600,h_time),x[4]))
-  #(sum_kernel,sum_kernel * temperature) 
-  kernel_sum = kernel_all.map(lambda x:(x[0],x[0]*x[1]))
-  predict_value = kernel_sum.reduce(lambda a,b: (a[0]+b[0],a[1]+b[1]))
-  prediction_temp[time]=kernel[1]/kernel[0]
+  #filter the dates which are posterior	
+  #(station_num,datetime,temperature)
+  temp_filter = temp_data.filter(lambda x: x[1]<datetime_interest)
+  #(kernel_all,temperatures)
+  kernel_all = temp_filter.map(lambda x: (kernel_distance(x[0])+kernel_day(x[1],datetime_interest)+kernel_hour(x[1],datetime_interest),x[2]))
+  #(sum:kernel,sum:kernel * temperature) 
+  kernel_sum = kernel_all.map(lambda x: (x[0],x[0]*x[1])).reduce(lambda a,b: (a[0]+b[0],a[1]+b[1]))
+  prediction_temp[time]=kernel_sum[1]/kernel_sum[0]
 
-list(prediction_temp.items()).rdd.saveAsTextFile("BDA/output")
-
+sc.parallelize(prediction_temp.items()).saveAsTextFile("BDA/output")
 
 
 
