1cbc94abdfc61edc8322d813e3fcb7de240d6c9d
rui0027 <90400187+rui0027@users.noreply.github.com>
2022-06-10 15:09:12 +0200

    Update parameter.py

--- lab3/parameter.py
+++ lab3/parameter.py
@@ -65,24 +65,29 @@ test_h_hour=[0.5,1,1.5,2,2.5,3,3.5,4,5,6,7,10]
 error_distance = []
 error_day = []
 error_hour = []
+
+def predict(k_distance,k_day,k_hour,test_date):
+	#filter the dates which are posterior	
+  	#(station_num,datetime,temperature)
+  	temp_train = temp_train.filter(lambda x: x[1]<test_date[1])
+  	#(kernel_diatance, kernel_day, kernel_date,temperature)
+  	kernel_all = temp_train.map(lambda x: (kernel_distance(x[0],test_date[0],k_distance),kernel_day(test_date[1],x[1],k_day),kernel_hour(test_date[1],x[1],k_hour),x[2]))
+  	#(kernel_diatance, kernel_day, kernel_date,kdistance*temp,kday*temp, khou*temp) 
+  	kernel_sum = kernel_all.map(lambda x: (x[0],x[1],x[2],x[0]*x[3],x[1]*x[3],x[2]*x[3]))
+  	kernel_sum = kernel_sum.reduce(lambda a,b: (a[0]+b[0],a[1]+b[1],a[2]+b[2],a[3]+b[3],a[4]+b[4],a[5]+b[5]))
+  	#(kernel_distance prediction,kernel_day prediction,kernel_hour prediction)
+	return (kernel_sum[3]/kernel_sum[0],kernel_sum[4]/kernel_sum[1],kernel_sum[5]/kernel_sum[2]) 
+	
 for in range(12):
 	error=(0,0,0)
-	for test_date in temp_test.collect():
-		#filter the dates which are posterior	
-  		#(station_num,datetime,temperature)
-  		temp_train = temp_train.filter(lambda x: x[1]<test_date[1])
-  		#(kernel_diatance, kernel_day, kernel_date,temperature)
-  		kernel_all = temp_train.map(lambda x: (kernel_distance(x[0],test_date[0],test_h_d[i]),kernel_day(test_date[1],x[1],test_h_day[i]),kernel_hour(test_date[1],x[1],test_h_hour[i]),x[2]))
-  		#(kernel_diatance, kernel_day, kernel_date,kdistance*temp,kday*temp, khou*temp) 
-  		kernel_sum = kernel_all.map(lambda x: (x[0],x[1],x[2],x[0]*x[3],x[1]*x[3],x[2]*x[3]))
-  		kernel_sum = kernel_sum.reduce(lambda a,b: (a[0]+b[0],a[1]+b[1],a[2]+b[2],a[3]+b[3],a[4]+b[4],a[5]+b[5]))
-  		#(kernel_distance prediction,kernel_day prediction,kernel_hour prediction)
-		kernel_predict = (kernel_sum[3]/kernel_sum[0],kernel_sum[4]/kernel_sum[1],kernel_sum[5]/kernel_sum[2]) 
-  		error = (error[0]+abs(test_date[2]-kernel_predict[0]),error[1]+abs(test_date[2]-kernel_predict[1]),error[2]+abs(test_date[2]-kernel_predict[2]))
+	#(temperature, prediction)
+	temp_pre=temp_test.map(lambda x: (x[2],predict(k_distance,k_day,k_hour,x)))
+	temp_error=temp_pre.map(lambda x: (x[1][0]-x[0],x[1][1]-x[0],x[1][2]-x[0]))
+	error=temp_error.reduce(lambda a,b: (a[0]+b[0],a[1]+b[1],a[2]+b[2]))
 	error_distance.append(error[0])
 	error_day.append(error[1])
 	error_hour.append(error[2])
 
-parameters = [test_h_d[argmin(error_distance)],test_h_day[argmin(error_day)],test_h_hour[argmin(error_hour)]]
+parameters = [test_h_d[np.array(error_distance).argmin()],test_h_day[np.array(error_day).argmin()],test_h_hour[np.array(error_hour).argmin()]]
 
 sc.parallelize(parameters).saveAsTextFile("BDA/output")
